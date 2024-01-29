b2574f808d2ee8c2a24f39c801c311563107a88c
koga <kosloski.fkoga@gmail.com>
2024-01-29 17:39:11 -0300

    build: change version code formula

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -12,4 +12,5 @@ dependencies {
 
     implementation(libs.android.gradle.plugin)
     implementation(libs.kotlin.gradle.plugin)
+    implementation(libs.kotlinx.datetime)
 }
--- build-logic/src/main/kotlin/AppInfo.kt
+++ build-logic/src/main/kotlin/AppInfo.kt
@@ -1,13 +1,34 @@
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+
 object Configuration {
     private const val majorVersion = 1
     private const val minorVersion = 0
     private const val patchVersion = 0
 
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-    const val versionCode = majorVersion * 10000 + minorVersion * 100 + patchVersion
+    val versionCode = (majorVersion * 10000) +
+            (minorVersion * 100) +
+            patchVersion +
+            getDateVersionBuild()
 
     const val targetSdk = 34
     const val compileSdk = 34
     const val minSdk = 24
     const val jvmTarget = "1.8"
+
+
+    private fun getDateVersionBuild(): Int {
+        val instant: Instant = Clock.System.now()
+        val localDateTime = instant.toLocalDateTime(TimeZone.currentSystemDefault())
+
+        val year = localDateTime.year % 100
+        val month = localDateTime.monthNumber
+        val day = localDateTime.dayOfMonth
+        val hour = localDateTime.hour
+
+        return year * 1000000 + month * 10000 + day * 100 + hour
+    }
 }
