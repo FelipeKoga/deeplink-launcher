d7d3bbfe9127e655d708d7da9863e33af86065a9
koga <kosloski.fkoga@gmail.com>
2024-01-29 16:51:02 -0300

    build: add formula to calculate versionCode

--- build-logic/src/main/kotlin/AppInfo.kt
+++ build-logic/src/main/kotlin/AppInfo.kt
@@ -1,6 +1,11 @@
 object Configuration {
-    const val versionName = "1.0.0"
-    const val versionCode = 1
+    private const val majorVersion = 1
+    private const val minorVersion = 0
+    private const val patchVersion = 0
+
+    const val versionName = "$majorVersion.$minorVersion.$patchVersion"
+    const val versionCode = majorVersion * 10000 + minorVersion * 100 + patchVersion
+
     const val targetSdk = 34
     const val compileSdk = 34
     const val minSdk = 24
