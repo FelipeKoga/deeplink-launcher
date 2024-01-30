b8d795c8c1de9bed04b06fbc79c8ca0207f97ae7
koga <kosloski.fkoga@gmail.com>
2024-01-30 12:20:33 -0300

    chore(design-system): extract color and shape to separated file

--- core/designsystem/src/commonMain/kotlin/dev/koga/deeplinklauncher/theme/DLLTheme.kt
+++ core/designsystem/src/commonMain/kotlin/dev/koga/deeplinklauncher/theme/DLLTheme.kt
@@ -1,33 +1,12 @@
 package dev.koga.deeplinklauncher.theme
 
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Shapes
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun DLLTheme(
     content: @Composable () -> Unit,
 ) {
-    val colors = darkColorScheme(
-        primary = Color.White,
-//        secondary = Color.Gray,
-        onPrimary = Color.Black,
-        onSecondary = Color.White,
-        background = Color(0xFF323232),
-        surface = Color(0xFF28272a),
-        secondaryContainer = Color(0xFF1c1b1f),
-    )
-
-    val shapes = Shapes(
-        small = RoundedCornerShape(4.dp),
-        medium = RoundedCornerShape(4.dp),
-        large = RoundedCornerShape(0.dp),
-    )
-
     MaterialTheme(
         colorScheme = colors,
         typography = typography,
