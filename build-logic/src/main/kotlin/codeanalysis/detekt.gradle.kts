cf68bbb1c676ce76b4023fcb36a769329d574548
koga <kosloski.fkoga@gmail.com>
2024-01-30 11:34:53 -0300

    build: add compose-rules

--- build-logic/src/main/kotlin/codeanalysis/detekt.gradle.kts
+++ build-logic/src/main/kotlin/codeanalysis/detekt.gradle.kts
@@ -1,21 +1,15 @@
 package codeanalysis
 
 import extension.getLibrary
-import io.gitlab.arturbosch.detekt.Detekt
 import io.gitlab.arturbosch.detekt.DetektPlugin
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
-import org.gradle.kotlin.dsl.apply
-import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.getByType
-import org.gradle.kotlin.dsl.withType
 
 val libs: VersionCatalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
 
 apply<DetektPlugin>()
 
 dependencies {
-    "detektPlugins"(libs.getLibrary("detekt"))
+    "detektPlugins"(libs.getLibrary("composerules"))
 }
 
 configure<DetektExtension> {
