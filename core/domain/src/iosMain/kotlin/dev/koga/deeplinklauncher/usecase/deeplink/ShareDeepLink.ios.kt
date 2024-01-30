2dad81895ab95e18adfc4f2a5917bdfc0d948fca
koga <kosloski.fkoga@gmail.com>
2024-01-30 00:05:12 -0300

    chore: add share deeplink usecases

---
+++ core/domain/src/iosMain/kotlin/dev/koga/deeplinklauncher/usecase/deeplink/ShareDeepLink.ios.kt
@@ -0,0 +1,8 @@
+package dev.koga.deeplinklauncher.usecase.deeplink
+
+import dev.koga.deeplinklauncher.model.DeepLink
+
+actual class ShareDeepLink {
+    actual operator fun invoke(deepLink: DeepLink) {
+    }
+}
