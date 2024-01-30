ac5a33d747026085bd6b442be3188c1dfd532e7b
koga <kosloski.fkoga@gmail.com>
2024-01-30 12:20:33 -0300

    chore(design-system): extract color and shape to separated file

---
+++ core/designsystem/src/commonMain/kotlin/dev/koga/deeplinklauncher/theme/Color.kt
@@ -0,0 +1,13 @@
+package dev.koga.deeplinklauncher.theme
+
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.ui.graphics.Color
+
+val colors = darkColorScheme(
+    primary = Color.White,
+    onPrimary = Color.Black,
+    onSecondary = Color.White,
+    background = Color(0xFF323232),
+    surface = Color(0xFF28272a),
+    secondaryContainer = Color(0xFF1c1b1f),
+)
