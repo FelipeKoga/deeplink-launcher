5846baf3edd9b1c16aa71e2271079982f53a4da2
koga <kosloski.fkoga@gmail.com>
2024-01-30 01:08:41 -0300

    feat: setup firebase crashlytics

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,13 @@ buildscript {
 
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    alias(libs.plugins.androidApplication).apply(false)
-    alias(libs.plugins.androidLibrary).apply(false)
-    alias(libs.plugins.kotlinAndroid).apply(false)
-    alias(libs.plugins.kotlinMultiplatform).apply(false)
-    alias(libs.plugins.sqlDelight).apply(false)
-    alias(libs.plugins.org.jetbrains.kotlin.jvm).apply(false)
-    alias(libs.plugins.jetbrainsCompose).apply(false)
+    alias(libs.plugins.androidApplication) apply(false)
+    alias(libs.plugins.androidLibrary) apply(false)
+    alias(libs.plugins.kotlinAndroid) apply(false)
+    alias(libs.plugins.kotlinMultiplatform) apply(false)
+    alias(libs.plugins.sqlDelight) apply(false)
+    alias(libs.plugins.org.jetbrains.kotlin.jvm) apply(false)
+    alias(libs.plugins.jetbrainsCompose) apply(false)
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.crashlytics) apply false
 }
