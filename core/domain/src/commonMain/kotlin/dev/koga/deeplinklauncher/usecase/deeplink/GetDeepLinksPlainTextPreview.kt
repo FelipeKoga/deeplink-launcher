6ff3391d4d6a14d00425f27616d9e60d48b346c2
koga <kosloski.fkoga@gmail.com>
2024-01-30 00:00:46 -0300

    fix(preview): remove default deeplink from preview

--- core/domain/src/commonMain/kotlin/dev/koga/deeplinklauncher/usecase/deeplink/GetDeepLinksPlainTextPreview.kt
+++ core/domain/src/commonMain/kotlin/dev/koga/deeplinklauncher/usecase/deeplink/GetDeepLinksPlainTextPreview.kt
@@ -1,12 +1,15 @@
 package dev.koga.deeplinklauncher.usecase.deeplink
 
+import dev.koga.deeplinklauncher.constant.defaultDeepLink
 import dev.koga.deeplinklauncher.datasource.DeepLinkDataSource
 
 class GetDeepLinksPlainTextPreview(
     private val dataSource: DeepLinkDataSource,
 ) {
     operator fun invoke(): String {
-        val deepLinks = dataSource.getDeepLinks()
+        val deepLinks = dataSource.getDeepLinks().filter {
+            it != defaultDeepLink
+        }
 
         return deepLinks.joinToString(separator = "\n") { deepLink ->
             deepLink.link
