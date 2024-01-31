78d016e6adcc2a4b312fc95f1721e8c900719343
koga <kosloski.fkoga@gmail.com>
2024-01-30 22:50:45 -0300

    feat(import/export): change import and export structure

--- core/domain/src/commonMain/kotlin/dev/koga/deeplinklauncher/usecase/deeplink/GetDeepLinksJsonPreview.kt
+++ core/domain/src/commonMain/kotlin/dev/koga/deeplinklauncher/usecase/deeplink/GetDeepLinksJsonPreview.kt
@@ -2,55 +2,68 @@ package dev.koga.deeplinklauncher.usecase.deeplink
 
 import dev.koga.deeplinklauncher.constant.defaultDeepLink
 import dev.koga.deeplinklauncher.datasource.DeepLinkDataSource
+import dev.koga.deeplinklauncher.datasource.FolderDataSource
+import dev.koga.deeplinklauncher.dto.ImportDto
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 class GetDeepLinksJsonPreview(
-    private val dataSource: DeepLinkDataSource,
+    private val deepLinkDataSource: DeepLinkDataSource,
+    private val folderDataSource: FolderDataSource
 ) {
 
+    operator fun invoke(): String {
+        val deepLinks = deepLinkDataSource.getDeepLinks().filter { it.id != defaultDeepLink.id }
+        val folders = folderDataSource.getFolders()
+
+        if (deepLinks.isEmpty() && folders.isEmpty()) return ""
+
+        val json = Json { prettyPrint = true }
+
+        return json.encodeToString(
+            ExportJson(
+                folders = folders.map {
+                    ExportJson.Folder(
+                        id = it.id,
+                        name = it.name,
+                        description = it.description,
+                    )
+                },
+                deepLinks = deepLinks.map {
+                    ExportJson.DeepLink(
+                        id = it.id,
+                        link = it.link,
+                        name = it.name,
+                        description = it.description,
+                        isFavorite = it.isFavorite,
+                        folderId = it.folder?.id,
+                    )
+                }
+            )
+        )
+    }
+
     @Serializable
-    private data class DeepLinkJson(
-        val id: String,
-        val link: String,
-        val name: String?,
-        val description: String?,
-        val isFavorite: Boolean,
-        val folder: FolderJson?,
+    private data class ExportJson(
+        val folders: List<Folder>,
+        val deepLinks: List<DeepLink>,
     ) {
         @Serializable
-        data class FolderJson(
+        data class DeepLink(
+            val id: String,
+            val link: String,
+            val name: String?,
+            val description: String?,
+            val isFavorite: Boolean,
+            val folderId: String?,
+        )
+
+        @Serializable
+        data class Folder(
             val id: String,
             val name: String,
             val description: String?,
         )
-    }
-
-    operator fun invoke(): String {
-        val deepLinks = dataSource.getDeepLinks()
-            .filter { it.id != defaultDeepLink.id }
-            .map {
-                DeepLinkJson(
-                    id = it.id,
-                    link = it.link,
-                    name = it.name,
-                    description = it.description,
-                    isFavorite = it.isFavorite,
-                    folder = it.folder?.let { folder ->
-                        DeepLinkJson.FolderJson(
-                            id = folder.id,
-                            name = folder.name,
-                            description = folder.description,
-                        )
-                    },
-                )
-            }
-
-        if (deepLinks.isEmpty()) return ""
-
-        val json = Json { prettyPrint = true }
-
-        return json.encodeToString(deepLinks)
     }
 }
