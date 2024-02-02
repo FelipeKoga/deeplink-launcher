06858828a68ff93dc8c73bac96ac7f1741ba41fb
koga <kosloski.fkoga@gmail.com>
2024-02-02 14:25:20 -0300

    chore(shared): create screen registry in shared module

--- feature/deeplink-details/ui/src/commonMain/kotlin/dev/koga/deeplinklauncher/navigation/DeepLinkDetailsScreenModule.kt
+++
@@ -1,9 +0,0 @@
-package dev.koga.deeplinklauncher.navigation
-
-import cafe.adriel.voyager.core.registry.screenModule
-import dev.koga.deeplinklauncher.DeepLinkDetailsScreen
-import dev.koga.deeplinklauncher.SharedScreen
-
-val deepLinkDetailsScreenModule = screenModule {
-    register<SharedScreen.DeepLinkDetails> { provider -> DeepLinkDetailsScreen(provider.id) }
-}
