872fa79128dd7b12ff344ee020adea32f7f93f31
koga <kosloski.fkoga@gmail.com>
2024-02-08 23:35:56 -0300

    feat(deeplink-actions): align popup below copy button

--- core/sharedui/src/commonMain/kotlin/dev/koga/deeplinklauncher/deeplink/DeepLinkActionsRow.kt
+++ core/sharedui/src/commonMain/kotlin/dev/koga/deeplinklauncher/deeplink/DeepLinkActionsRow.kt
@@ -5,12 +5,9 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -26,16 +23,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.LayoutCoordinates
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
 import androidx.compose.ui.window.PopupProperties
@@ -56,6 +57,19 @@ fun DeepLinkActionsRow(
     val clipboardManager = LocalClipboardManager.current
 
     var showCopyPopUp by remember { mutableStateOf(false) }
+    var copyCoordinates by remember { mutableStateOf<LayoutCoordinates?>(null) }
+    val copyCoordinatesOffset by remember(copyCoordinates) {
+        derivedStateOf {
+            val extraPadding = 62
+            val position = copyCoordinates?.positionInParent()
+            val height = copyCoordinates?.size?.height ?: 0
+
+            IntOffset(
+                x = position?.x?.toInt() ?: 0,
+                y = (position?.y?.toInt() ?: 0) + height + extraPadding
+            )
+        }
+    }
 
     LaunchedEffect(showCopyPopUp) {
         if (showCopyPopUp) {
@@ -64,92 +78,100 @@ fun DeepLinkActionsRow(
         }
     }
 
-    Row(
-        horizontalArrangement = Arrangement.SpaceAround,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 24.dp),
-    ) {
-        FilledTonalButton(onClick = onDuplicate) {
-            Text(
-                text = "Duplicate",
-                style = MaterialTheme.typography.labelLarge.copy(
-                    fontWeight = FontWeight.Bold
-                )
-            )
-        }
 
-        Spacer(modifier = Modifier.width(16.dp))
+    Row(modifier = Modifier.fillMaxWidth()) {
 
-        IconButton(onClick = onShare) {
-            Icon(
-                imageVector = Icons.Rounded.Share,
-                contentDescription = "Share",
-            )
-        }
-
-        IconButton(onClick = onFavorite) {
-            Icon(
-                imageVector = if (isFavorite) {
-                    Icons.Rounded.Favorite
-                } else {
-                    Icons.Rounded.FavoriteBorder
-                },
-                contentDescription = "Favorite",
-                tint = if (isFavorite) {
-                    Color(0xFFE57373)
-                } else {
-                    MaterialTheme.colorScheme.onSurface
-                },
-            )
-        }
-
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
+        AnimatedVisibility(
+            visible = showCopyPopUp,
+            enter = fadeIn(initialAlpha = 0.3f),
+            exit = fadeOut(targetAlpha = 0.0f)
         ) {
-            IconButton(onClick = {
-                clipboardManager.setText(AnnotatedString(link))
-                showCopyPopUp = true
-            }) {
+            Popup(
+                offset = copyCoordinatesOffset,
+                properties = PopupProperties(
+                    focusable = false,
+                    dismissOnBackPress = true,
+                    dismissOnClickOutside = true
+                ),
+            ) {
+                Text(
+                    text = "Copied!",
+                    style = MaterialTheme.typography.labelMedium.copy(
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.onPrimary
+                    ),
+                    modifier = Modifier
+                        .background(
+                            MaterialTheme.colorScheme.primary,
+                            RoundedCornerShape(12.dp)
+                        )
+                        .padding(vertical = 4.dp, horizontal = 8.dp)
+                )
+            }
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 24.dp),
+        ) {
+            FilledTonalButton(onClick = onDuplicate) {
+                Text(
+                    text = "Duplicate",
+                    style = MaterialTheme.typography.labelLarge.copy(
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            IconButton(onClick = onShare) {
+                Icon(
+                    imageVector = Icons.Rounded.Share,
+                    contentDescription = "Share",
+                )
+            }
+
+            IconButton(onClick = onFavorite) {
+                Icon(
+                    imageVector = if (isFavorite) {
+                        Icons.Rounded.Favorite
+                    } else {
+                        Icons.Rounded.FavoriteBorder
+                    },
+                    contentDescription = "Favorite",
+                    tint = if (isFavorite) {
+                        Color(0xFFE57373)
+                    } else {
+                        MaterialTheme.colorScheme.onSurface
+                    },
+                )
+            }
+
+            IconButton(
+                modifier = Modifier.onGloballyPositioned {
+                    copyCoordinates = it
+                },
+                onClick = {
+                    clipboardManager.setText(AnnotatedString(link))
+                    showCopyPopUp = true
+                }) {
                 Icon(
                     painter = painterResource(MR.images.ic_content_copy_24dp),
                     contentDescription = "Copy Link",
                 )
             }
 
-            AnimatedVisibility(
-                visible = showCopyPopUp,
-                enter = fadeIn(initialAlpha = 0.3f), // Customize as needed
-                exit = fadeOut(targetAlpha = 0.0f) // Customize as needed
-            ) {
-                Popup(
-                    properties = PopupProperties(focusable = false),
-                ) {
-                    Text(
-                        text = "Copied!",
-                        style = MaterialTheme.typography.labelMedium.copy(
-                            fontWeight = FontWeight.Bold,
-                            color = MaterialTheme.colorScheme.onPrimary
-                        ),
-                        modifier = Modifier
-                            .background(
-                                MaterialTheme.colorScheme.primary,
-                                RoundedCornerShape(12.dp)
-                            )
-                            .padding(vertical = 4.dp, horizontal = 8.dp)
-                    )
-                }
+            FilledTonalIconButton(onClick = onLaunch) {
+                Icon(
+                    painter = painterResource(MR.images.ic_launch_24dp),
+                    contentDescription = "Launch",
+                )
             }
-
-        }
-
-
-
-        FilledTonalIconButton(onClick = onLaunch) {
-            Icon(
-                painter = painterResource(MR.images.ic_launch_24dp),
-                contentDescription = "Launch",
-            )
         }
     }
+
+
 }
