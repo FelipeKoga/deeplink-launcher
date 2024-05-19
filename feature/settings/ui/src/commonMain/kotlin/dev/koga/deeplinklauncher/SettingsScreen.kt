b8358a276bd6d52241e0a68e5e6c1e1589ffc212
koga <kosloski.fkoga@gmail.com>
2024-05-19 16:40:53 -0300

    chore: improve folder selection ui

--- feature/settings/ui/src/commonMain/kotlin/dev/koga/deeplinklauncher/SettingsScreen.kt
+++ feature/settings/ui/src/commonMain/kotlin/dev/koga/deeplinklauncher/SettingsScreen.kt
@@ -1,7 +1,6 @@
 package dev.koga.deeplinklauncher
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
