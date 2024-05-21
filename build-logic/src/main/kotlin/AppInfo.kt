2f1189bca2d356e0f21b298375d4025ccf89e3ce
koga <kosloski.fkoga@gmail.com>
2024-05-20 23:11:58 -0300

    build: bump version to 1.4.0

--- build-logic/src/main/kotlin/AppInfo.kt
+++ build-logic/src/main/kotlin/AppInfo.kt
@@ -2,7 +2,7 @@ import kotlinx.datetime.Clock
 
 object Configuration {
     private const val MAJOR_VERSION = 1
-    private const val MINOR_VERSION = 3
+    private const val MINOR_VERSION = 4
     private const val PATCH_VERSION = 0
 
     const val VERSION_NAME = "$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION"
