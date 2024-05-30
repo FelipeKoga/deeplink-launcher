7ea5558d993de983143f31e8f36ddfe7d69fb02a
koga <kosloski.fkoga@gmail.com>
2024-05-30 11:34:49 -0300

    chore: remove all submodules && migrate to specific modules

--- core/domain/src/commonMain/kotlin/dev/koga/deeplinklauncher/di/DomainModule.kt
+++ core/domain/src/commonMain/kotlin/dev/koga/deeplinklauncher/di/DomainModule.kt
@@ -4,6 +4,9 @@ import dev.koga.deeplinklauncher.usecase.ExportDeepLinks
 import dev.koga.deeplinklauncher.usecase.GetDeepLinksJsonPreview
 import dev.koga.deeplinklauncher.usecase.GetDeepLinksPlainTextPreview
 import dev.koga.deeplinklauncher.usecase.DuplicateDeepLink
+import dev.koga.deeplinklauncher.usecase.GetAutoSuggestionLinks
+import dev.koga.deeplinklauncher.usecase.GetDeepLinksAndFolderStream
+import dev.koga.deeplinklauncher.usecase.ImportDeepLinks
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -13,6 +16,9 @@ val domainModule = module {
     singleOf(::ExportDeepLinks)
     singleOf(::GetDeepLinksJsonPreview)
     singleOf(::GetDeepLinksPlainTextPreview)
+    singleOf(::GetDeepLinksAndFolderStream)
+    singleOf(::GetAutoSuggestionLinks)
+    singleOf(::ImportDeepLinks)
 
     includes(platformDomainModule)
 }
