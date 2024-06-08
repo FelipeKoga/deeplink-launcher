1ff8bb8470d0740a67afa00da7817f8366520b0d
koga <kosloski.fkoga@gmail.com>
2024-06-08 17:03:52 -0300

    feat: adjust app navigation

--- core/navigation/src/commonMain/kotlin/dev/koga/deeplinklauncher/NavigatorExt.kt
+++ core/navigation/src/commonMain/kotlin/dev/koga/deeplinklauncher/NavigatorExt.kt
@@ -1,12 +1,8 @@
 package dev.koga.deeplinklauncher
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.mutableStateMapOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.staticCompositionLocalOf
 import cafe.adriel.voyager.core.registry.ScreenRegistry
-import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.bottomSheet.BottomSheetNavigator
 
@@ -15,51 +11,6 @@ fun BottomSheetNavigator.navigateToDeepLinkDetails(id: String, showFolder: Boole
     show(screen)
 }
 
-
-private val results = mutableStateMapOf<String, Any?>()
-
-
-fun Navigator.popWithResult(key: String, result: Any?) {
-    results[key] = result
-    pop()
-}
-
-fun Navigator.popUntilWithResult(predicate: (Screen) -> Boolean, result: Any?) {
-    val currentScreen = lastItem
-    results[currentScreen.key] = result
-    popUntil(predicate)
-}
-
-fun Navigator.clearResults() {
-    results.clear()
-}
-
-@Composable
-fun <T> Navigator.getResult(screenKey: String): State<T?> {
-    val result = results[screenKey] as? T
-    val resultState = remember(screenKey, result) {
-        derivedStateOf {
-            results.remove(screenKey)
-            result
-        }
-    }
-    return resultState
-}
-
-fun BottomSheetNavigator.hideWithResult(result: Any?, key: String) {
-    results[key] = result
-    this.hide()
-
-}
-
-@Composable
-fun <T> BottomSheetNavigator.getResult(screenKey: String): State<T?> {
-    val result = results[screenKey] as? T
-    val resultState = remember(screenKey, result) {
-        derivedStateOf {
-            results -= screenKey
-            result
-        }
-    }
-    return resultState
+val LocalRootNavigator = staticCompositionLocalOf<Navigator> {
+    error("No RootNavigator provided")
 }
--- core/navigation/src/commonMain/kotlin/dev/koga/deeplinklauncher/SharedScreen.kt
+++ core/navigation/src/commonMain/kotlin/dev/koga/deeplinklauncher/SharedScreen.kt
@@ -5,17 +5,7 @@ import cafe.adriel.voyager.core.registry.ScreenProvider
 sealed interface SharedScreen : ScreenProvider {
     data object ExportDeepLinks : SharedScreen
     data object ImportDeepLinks : SharedScreen
-    data class DeepLinkDetails(val id: String, val showFolder: Boolean) : SharedScreen {
-        sealed interface Result {
-            data class NavigateToFolderDetails(val id: String) : Result
-            data class NavigateToDuplicated(val id: String) : Result
-
-            companion object {
-                const val KEY = "deeplink_details_result"
-            }
-        }
-    }
-
+    data class DeepLinkDetails(val id: String, val showFolder: Boolean) : SharedScreen
     data class FolderDetails(val id: String) : SharedScreen
     data object Settings : SharedScreen
 }
