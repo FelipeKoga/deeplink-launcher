bbe7a575e0c9a44c63b71c0b3984427dc116d96b
Irineu333 <mmmirineusilva@gmail.com>
2024-08-16 10:36:44 -0300

    test(launch-target): testing parser

---
+++ core/domain/src/jvmTest/kotlin/dev/koga/deeplinklauncher/usecase/DeviceParserTest.kt
@@ -0,0 +1,42 @@
+package dev.koga.deeplinklauncher.usecase
+
+import dev.koga.deeplinklauncher.model.Target
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class DeviceParserTest {
+
+    private val parser = DeviceParser()
+
+    @Test
+    fun `testing one emulator`() {
+
+        val target = listOf(
+            "emulator-5554 device",
+            "emulator-5554 offline",
+            "0001ZF523HKK7K device",
+            "0001ZF523HKK7K offline",
+        )
+
+        val expected = listOf(
+            Target.Device.Emulator(
+                name = "emulator-5554",
+                active = true
+            ),
+            Target.Device.Emulator(
+                name = "emulator-5554",
+                active = false
+            ),
+            Target.Device.Physical(
+                name = "ZF523HKK7K",
+                active = true
+            ),
+            Target.Device.Physical(
+                name = "ZF523HKK7K",
+                active = false
+            ),
+        )
+
+        assertEquals(expected, target.map(parser::invoke))
+    }
+}
