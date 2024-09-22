1a8556935bd4e0f5208f2311eb730b18649c9e5d
koga <39222036+FelipeKoga@users.noreply.github.com>
2024-09-21 18:13:23 -0700

    fix(desktop): auto suggestion not working (#55)
    * fix(deps): downgrade gradle and agp versions
    
    * chore(desktop): rename browser target to desktop
    
    * chore(desktop): add desktop targets icons
    
    * chore: remove fontawesome
    
    * style: fix lint issues
    
    * fix(desktop): deeplink metadata logic not working properly
    
    * style: remove unused comment
    
    * style: fix lint issues

--- core/domain/src/jvmMain/kotlin/dev/koga/deeplinklauncher/usecase/GetDeepLinkMetadata.jvm.kt
+++ core/domain/src/jvmMain/kotlin/dev/koga/deeplinklauncher/usecase/GetDeepLinkMetadata.jvm.kt
@@ -2,15 +2,23 @@ package dev.koga.deeplinklauncher.usecase
 
 actual class GetDeepLinkMetadata {
     actual fun execute(link: String): DeepLinkMetadata {
-        val scheme = link.substringBefore("://", "")
-        val host = link.substringAfter("://", "")
-        val query = host.substringAfter("?", "")
+        val scheme = link.substringBefore(':', "").takeIf { it.isNotBlank() }
+
+        val remainingLink = link.substringAfter(':', "")
+
+        val host = if (remainingLink.startsWith("//")) {
+            remainingLink.substringAfter("//").substringBefore("?")
+        } else {
+            remainingLink.substringBefore("?")
+        }
+
+        val query = remainingLink.substringAfter("?", "").takeIf { it.isNotBlank() }
 
         return DeepLinkMetadata(
             link = link,
-            scheme = scheme.ifEmpty { null },
-            query = query.ifEmpty { null },
+            scheme = scheme,
             host = host.ifEmpty { null },
+            query = query,
         )
     }
 }
