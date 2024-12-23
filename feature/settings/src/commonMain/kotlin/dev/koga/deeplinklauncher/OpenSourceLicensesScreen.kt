5605c8492a9f2d9847cfcf78ad02ba11e85c817f
koga <39222036+FelipeKoga@users.noreply.github.com>
2024-12-22 22:23:23 -0300

    fix: open source licenses screen crash (#74)
    * fix(open-source-licenses): add screen key to prevent transition exception
    
    * chore(open-source-licenses): update aboutlibraries.json

--- feature/settings/src/commonMain/kotlin/dev/koga/deeplinklauncher/OpenSourceLicensesScreen.kt
+++ feature/settings/src/commonMain/kotlin/dev/koga/deeplinklauncher/OpenSourceLicensesScreen.kt
@@ -7,12 +7,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.core.screen.ScreenKey
 import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
 import com.mikepenz.aboutlibraries.ui.compose.m3.rememberLibraries
 import dev.koga.resources.Res
 import org.jetbrains.compose.resources.ExperimentalResourceApi
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 object OpenSourceLicensesScreen : Screen {
+
+    @OptIn(ExperimentalUuidApi::class)
+    override val key: ScreenKey
+        get() = Uuid.random().toString()
 
     @OptIn(ExperimentalMaterial3Api::class, ExperimentalResourceApi::class)
     @Composable
