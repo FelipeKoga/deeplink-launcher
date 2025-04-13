0ff0ee0268bafac9b550a50dac02ec1f7d7aa7cb
koga <felipe.koga@hotmail.com>
2025-04-13 19:17:28 -0300

    chore(ios): disable deeplink from clipboard

--- feature/deeplink/impl/src/iosMain/kotlin/dev/koga/deeplinklauncher/deeplink/impl/usecase/GetDeepLinkFromClipboard.kt
+++ feature/deeplink/impl/src/iosMain/kotlin/dev/koga/deeplinklauncher/deeplink/impl/usecase/GetDeepLinkFromClipboard.kt
@@ -1,16 +1,7 @@
 package dev.koga.deeplinklauncher.deeplink.impl.usecase
 
-import dev.koga.deeplinklauncher.deeplink.api.usecase.ValidateDeepLink
-import platform.UIKit.UIPasteboard
-
-actual class GetDeepLinkFromClipboard(
-    private val validateDeepLink: ValidateDeepLink,
-) {
+actual class GetDeepLinkFromClipboard {
     actual operator fun invoke(): String? {
-        val text = UIPasteboard.generalPasteboard.string ?: return null
-
-        if (!validateDeepLink.isValid(text)) return null
-
-        return text
+        return null
     }
 }
