7e364362e470bd69830f897771005c8221bd72b1
koga <39222036+FelipeKoga@users.noreply.github.com>
2025-04-23 20:27:06 -0300

    chore: improve database initialization (#161)

--- core/database/src/commonMain/kotlin/dev/koga/deeplinklauncher/database/DatabaseProvider.kt
+++ core/database/src/commonMain/kotlin/dev/koga/deeplinklauncher/database/DatabaseProvider.kt
@@ -5,13 +5,7 @@ import dev.koga.deeplinklauncher.database.converter.localDateTimeAdapter
 internal class DatabaseProvider(
     private val driverFactory: DriverFactory,
 ) {
-
-    private var database: DeepLinkLauncherDatabase? = null
-
-    fun getInstance(): DeepLinkLauncherDatabase =
-        database ?: createDatabase().also { database = it }
-
-    private fun createDatabase(): DeepLinkLauncherDatabase {
+    fun create(): DeepLinkLauncherDatabase {
         val driver = driverFactory.createDriver(databaseName = DATABASE_NAME)
 
         val database = DeepLinkLauncherDatabase(
