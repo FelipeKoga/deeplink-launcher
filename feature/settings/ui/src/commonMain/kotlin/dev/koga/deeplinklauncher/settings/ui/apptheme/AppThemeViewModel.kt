3d033c890ff2ce8dbe474ba7e334707f3f7e7a2e
koga <felipe.koga@hotmail.com>
2025-04-23 20:32:45 -0300

    chore: rename property

--- feature/settings/ui/src/commonMain/kotlin/dev/koga/deeplinklauncher/settings/ui/apptheme/AppThemeViewModel.kt
+++ feature/settings/ui/src/commonMain/kotlin/dev/koga/deeplinklauncher/settings/ui/apptheme/AppThemeViewModel.kt
@@ -7,15 +7,15 @@ import dev.koga.deeplinklauncher.preferences.repository.PreferencesDataSource
 import kotlinx.coroutines.launch
 
 class AppThemeViewModel(
-    private val repository: PreferencesDataSource,
+    private val dataSource: PreferencesDataSource,
     private val appCoroutineScope: AppCoroutineScope,
 ) : ViewModel() {
 
-    val appTheme = repository.preferences.appTheme
+    val appTheme = dataSource.preferences.appTheme
 
     fun update(appTheme: AppTheme) {
         appCoroutineScope.launch {
-            repository.updateTheme(appTheme)
+            dataSource.updateTheme(appTheme)
         }
     }
 }
